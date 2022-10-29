6c74f2d515490485b5d6b53037008a703b9c8fd3
Valentyn Ipolitov <sv.vartey@gmail.com>
2022-10-29 19:50:43 +0200

    temp redirect deleted

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,8 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (App::environment(['staging', 'production'])) {
-            URL::forceScheme('https');
-        }
+        // if (App::environment(['staging', 'production'])) {
+        //     URL::forceScheme('https');
+        // }
     }
 }
